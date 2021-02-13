ac8a6398e5f8b714a0bae119cc33e32d337d6284
Mostafa Rowghanian <mostafatech@outlook.com>
2021-02-13 20:51:38 +0330

    initial

---
+++ src/MCS.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCS.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
